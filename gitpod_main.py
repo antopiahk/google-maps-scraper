865a34949445b67789d04749f97b5ed0cc61c963
antopiahk <31695089+antopiahk@users.noreply.github.com>
2023-12-18 22:06:45 +0800

    futino changes and removing output from gitignore

---
+++ gitpod_main.py
@@ -0,0 +1,22 @@
+from src.gmaps import Gmaps
+from src.category import Category
+from city_queries import city_queries as city_queries_list
+
+star_it = '''Help us reach 850 stars, and we'll break the GMaps 120 limit, giving you 150+ to 250+ potential customers per search.
+             Star us to make it happen ⭐! https://github.com/omkarcloud/google-maps-scraper/'''
+ 
+queries = city_queries_list
+
+first_quarter = len(city_queries_list)//4
+second_quarter = round(len(city_queries_list)//(4/2))
+third_quarter = round(len(city_queries_list)//(4/3))
+fourth_quarter = len(city_queries_list)
+
+# gitpod originally was [167:] but credits ran out. this is picking up from around the finish.
+city_queries = city_queries_list[175:]
+
+city_queries = [item for sublist in city_queries for item in sublist]
+
+# city_queries.reverse()
+
+Gmaps.places(city_queries)
