43a213f99689fa8e15a1482a6bac815043e57b24
Jorge Lewis <31695089+antopiahk@users.noreply.github.com>
2023-12-19 12:22:52 +0800

    removing empty files

--- csv_stuff.py
+++ csv_stuff.py
@@ -10,25 +10,27 @@ def combine_csv_files(input_folder, output_file):
         for file in files:
             if file.endswith('.csv'):
                 file_path = os.path.join(root, file)
-                try:
-                    # Read each CSV file into a DataFrame and append to the list
-                    df = pd.read_csv(file_path)
-                    if not df.empty:
-                        # print(f'CSV file added to merge: {file_path}')
-                        all_csv_dataframes.append(df)
-                    else:
+                if os.path.exists(file_path):  # Check if the file exists
+                    try:
+                        # Read each CSV file into a DataFrame and append to the list
+                        df = pd.read_csv(file_path)
+                        if not df.empty:
+                            # print(f'CSV file added to merge: {file_path}')
+                            all_csv_dataframes.append(df)
+                        else:
+                            # If the file is empty, delete its parent folder
+                            delete_folder = os.path.dirname(os.path.dirname(file_path))
+                            if not 'all' in file_path and delete_folder != 'output':
+                                print(f'Deleting folder: {delete_folder}')
+                                shutil.rmtree(delete_folder)
+                    except pd.errors.EmptyDataError:
                         # If the file is empty, delete its parent folder
                         delete_folder = os.path.dirname(os.path.dirname(file_path))
                         if not 'all' in file_path and delete_folder != 'output':
                             print(f'Deleting folder: {delete_folder}')
                             shutil.rmtree(delete_folder)
-                except pd.errors.EmptyDataError:
-                    # If the file is empty, delete its parent folder
-                    delete_folder = os.path.dirname(os.path.dirname(file_path))
-                    if not 'all' in file_path and delete_folder != 'output':
-                        print(f'Deleting folder: {delete_folder}')
-                        # shutil.rmtree(delete_folder)
-
+                else:
+                    print(f'File not found: {file_path}')
     if all_csv_dataframes:
         # Combine all DataFrames into one
         combined_df = pd.concat(all_csv_dataframes, ignore_index=True)
